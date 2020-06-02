e95e8a7443d948852e9fabd66e34218fc5594a62
Wanlin-Lu <luwanlin@126.com>
2020-06-03 01:04:16 +0800

    fd over

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
 
-function App() {
+import Header from './components/Header/Header'
+import NewProduct from './components/Products/NewProduct'
+import ProductList from './components/Products/ProductList'
+import './App.css'
+
+const App = () => {
+  const [isLoading, setIsLoading] = useState(false)
+  const [loadedProducts, setLoadedProducts] = useState([])
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      setIsLoading(true)
+      const response = await fetch('http://localhost:5000/products')
+
+      const responseData = await response.json()
+
+      setLoadedProducts(responseData.products)
+      setIsLoading(false);
+    }
+
+    fetchProducts()
+  }, [])
+
+  const addProductHandler = async (productName, productPrice) => {
+    const product = {
+      name: productName,
+      price: +productPrice
+    }
+    try {
+      const response = await fetch('http://localhost:5000/products', {
+        method: 'POST',
+        body: JSON.stringify(product),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+
+      const responseData = await response.json()
+
+      setLoadedProducts(responseData.products)
+      
+    } catch (err) {
+      alert(err.message || 'Something went wrong!')
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <React.Fragment>
+      <Header />
+      <main>
+        <NewProduct onAddProduct={addProductHandler} />
+        {isLoading && <p className="loader">Loading...</p>}
+        {!isLoading && <ProductList products={loadedProducts} />}
+      </main>
+    </React.Fragment>
+  )
 }
 
-export default App;
+export default App
+
