64ceedce796754307aaf6e80cb8c304027c90b21
Wanlin-Lu <luwanlin@126.com>
2020-06-02 15:34:01 +0800

    set server

---
+++ server/server.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient
+
+const productsRouter = require('./routers/products-router')
+const productsDAO = require('./dao/productsDAO')
+
+const DB_URL = `mongodb+srv://luwanlin:R37T9XXsQivoY4pq@shareplaces-u7evs.gcp.mongodb.net/products_test?retryWrites=true&w=majority`;
+
+const app = express()
+
+app.use(bodyParser.json())
+
+app.use('/products', productsRouter)
+
+app.use((req, res, next) => {
+  throw new Error('Could not find this route.')
+})
+
+MongoClient.connect(
+  DB_URL,
+  {
+    useNewUrlParser: true,
+    poolSize: 50,
+    wtimeout: 2500,
+  }
+).catch(err => {
+  console.log(err.stack)
+  process.exit(1)
+}).then(async client => {
+  await productsDAO.injectDB(client)
+  app.listen(5000, () => {
+    console.log(`listening on port 5000`);
+  });
+})
